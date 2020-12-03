0877c0ee15340336b2c9b14acab43819bce9a2eb
Zé Santos <jpestima@protonmail.com>
2020-12-03 16:09:01 +0000

    Este custou ainda menos

--- src/main/kotlin/com/talkdesk/billstracker/repositories/BillsRepository.kt
+++ src/main/kotlin/com/talkdesk/billstracker/repositories/BillsRepository.kt
@@ -1,13 +1,10 @@
 package com.talkdesk.billstracker.repositories
 
 import com.talkdesk.billstracker.entities.Bill
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.Optional
+
 @Repository
-interface BillsRepository {
-	fun findById(id: Int): Optional<Bill>
-	fun findAll(): List<Bill>
-	fun deleteById(id: Int)
-	fun save(bill: Bill): Bill
-	fun deleteAll()
+interface BillsRepository : CrudRepository<Bill, Int> {
 }
