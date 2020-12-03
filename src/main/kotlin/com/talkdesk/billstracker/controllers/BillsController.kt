2f52d07efae0856472eb517eaef89dfbf6cc9d83
Zé Santos <jpestima@protonmail.com>
2020-12-03 14:24:58 +0000

    Integration post test

--- src/main/kotlin/com/talkdesk/billstracker/controllers/BillsController.kt
+++ src/main/kotlin/com/talkdesk/billstracker/controllers/BillsController.kt
@@ -4,27 +4,31 @@ import com.talkdesk.billstracker.entities.Bill
 import com.talkdesk.billstracker.services.BillsService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("bills")
+@RestController
 class BillsController(
-	private val billsService: BillsService
+        private val billsService: BillsService
 ) {
-	@GetMapping
-	fun all() = billsService.findAll()
+    @GetMapping
+    fun all() = billsService.findAll()
 
-	fun create(bill: Bill): ResponseEntity<Bill> =
-		ResponseEntity(billsService.create(bill), HttpStatus.CREATED)
+    @PostMapping
+    fun create(@RequestBody bill: Bill): ResponseEntity<Bill> =
+            ResponseEntity(billsService.create(bill), HttpStatus.CREATED)
 
-	fun get(id: Int): ResponseEntity<Bill?> =
-		ResponseEntity(billsService.findById(id), HttpStatus.OK)
+    @GetMapping("/{id}")
+    fun get(id: Int): ResponseEntity<Bill?> =
+            ResponseEntity(billsService.findById(id), HttpStatus.OK)
 
-	fun update(bill: Bill, id: Int): ResponseEntity<Bill> =
-		ResponseEntity(billsService.update(bill.copy(id = id)), HttpStatus.OK)
+    @PutMapping("/{id}")
+    fun update(@RequestBody bill: Bill, id: Int): ResponseEntity<Bill> =
+            ResponseEntity(billsService.update(bill.copy(id = id)), HttpStatus.OK)
 
-	fun delete(id: Int): ResponseEntity<Any> =
-		billsService.delete(id).run {
-			ResponseEntity(HttpStatus.NO_CONTENT)
-		}
+    @DeleteMapping("/{id}")
+    fun delete(id: Int): ResponseEntity<Any> =
+            billsService.delete(id).run {
+                ResponseEntity(HttpStatus.NO_CONTENT)
+            }
 }
--- src/main/kotlin/com/talkdesk/billstracker/services/BillsService.kt
+++ src/main/kotlin/com/talkdesk/billstracker/services/BillsService.kt
@@ -3,22 +3,24 @@ package com.talkdesk.billstracker.services
 import com.talkdesk.billstracker.entities.Bill
 import com.talkdesk.billstracker.exceptions.NotFoundException
 import com.talkdesk.billstracker.repositories.BillsRepository
+import org.springframework.stereotype.Service
 
+@Service
 class BillsService(
-	private val billsRepository: BillsRepository
+        private val BillsRepositoryImpl: BillsRepository
 ) {
-	fun findById(id: Int): Bill = billsRepository.findById(id).orElseThrow{
-		NotFoundException()
-	}
+    fun findById(id: Int): Bill = BillsRepositoryImpl.findById(id).orElseThrow {
+        NotFoundException()
+    }
 
-	fun findAll() = billsRepository.findAll()
+    fun findAll() = BillsRepositoryImpl.findAll()
 
-	fun delete(id: Int) {
-		findById(id).run {billsRepository.deleteById(id)}
+    fun delete(id: Int) {
+        findById(id).run { BillsRepositoryImpl.deleteById(id) }
 
-	}
+    }
 
-	fun create(bill: Bill): Bill = billsRepository.save(bill)
+    fun create(bill: Bill): Bill = BillsRepositoryImpl.save(bill)
 
-	fun update(bill: Bill): Bill = findById(bill.id!!).run{billsRepository.save(bill)}
+    fun update(bill: Bill): Bill = findById(bill.id!!).run { BillsRepositoryImpl.save(bill) }
 }
