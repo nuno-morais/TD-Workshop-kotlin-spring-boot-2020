76bfabd66d05bcf17d92cda79f7011b36ea3e3ab
jdportugal <jd_portugal@hotmail.com>
2020-12-03 14:20:51 +0000

    Finished first part

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
 
 	// Tests
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/kotlin/com/talkdesk/billstracker/entities/Bill.kt
+++ src/main/kotlin/com/talkdesk/billstracker/entities/Bill.kt
@@ -1,9 +1,15 @@
 package com.talkdesk.billstracker.entities
 
+import org.jetbrains.annotations.NotNull
+
+
 data class Bill(
 	val id: Int? = null,
+	@NotNull
 	val description: String,
+	@NotNull
 	val price: Int,
+	@NotNull
 	val accountId: String
 ) {
 	override fun toString() =
