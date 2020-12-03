a1d07e2db4e0d4bc2664d9f25f28929c7e9addf0
Zé Santos <jpestima@protonmail.com>
2020-12-03 12:23:12 +0000

    Unit test

--- src/main/kotlin/com/talkdesk/billstracker/services/BillsService.kt
+++ src/main/kotlin/com/talkdesk/billstracker/services/BillsService.kt
@@ -1,20 +1,24 @@
 package com.talkdesk.billstracker.services
 
 import com.talkdesk.billstracker.entities.Bill
+import com.talkdesk.billstracker.exceptions.NotFoundException
 import com.talkdesk.billstracker.repositories.BillsRepository
 
 class BillsService(
 	private val billsRepository: BillsRepository
 ) {
-	fun findById(id: Int): Bill = TODO()
+	fun findById(id: Int): Bill = billsRepository.findById(id).orElseThrow{
+		NotFoundException()
+	}
 
 	fun findAll() = billsRepository.findAll()
 
 	fun delete(id: Int) {
-		TODO()
+		findById(id).run {billsRepository.deleteById(id)}
+
 	}
 
-	fun create(bill: Bill): Bill = TODO()
+	fun create(bill: Bill): Bill = billsRepository.save(bill)
 
-	fun update(bill: Bill): Bill = TODO()
+	fun update(bill: Bill): Bill = findById(bill.id!!).run{billsRepository.save(bill)}
 }
