b83139e2b689b68c48d980c2813777c25f1bb225
João Martins <joao.martins@talkdesk.com>
2020-12-03 15:26:16 +0000

    Add missing annotations

--- src/main/kotlin/com/talkdesk/billstracker/controllers/BillsController.kt
+++ src/main/kotlin/com/talkdesk/billstracker/controllers/BillsController.kt
@@ -4,9 +4,9 @@ import com.talkdesk.billstracker.entities.Bill
 import com.talkdesk.billstracker.services.BillsService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 
+@RestController
 @RequestMapping("bills")
 class BillsController(
 	private val billsService: BillsService
@@ -14,16 +14,20 @@ class BillsController(
 	@GetMapping
 	fun all() = billsService.findAll()
 
-	fun create(bill: Bill): ResponseEntity<Bill> =
+	@PostMapping
+	fun create(@RequestBody bill: Bill): ResponseEntity<Bill> =
 		ResponseEntity(billsService.create(bill), HttpStatus.CREATED)
 
-	fun get(id: Int): ResponseEntity<Bill?> =
+	@GetMapping("/{id}")
+	fun get(@PathVariable id: Int): ResponseEntity<Bill?> =
 		ResponseEntity(billsService.findById(id), HttpStatus.OK)
 
-	fun update(bill: Bill, id: Int): ResponseEntity<Bill> =
+	@PutMapping("/{id}")
+	fun update(@RequestBody bill: Bill, @PathVariable id: Int): ResponseEntity<Bill> =
 		ResponseEntity(billsService.update(bill.copy(id = id)), HttpStatus.OK)
 
-	fun delete(id: Int): ResponseEntity<Any> =
+	@DeleteMapping("/{id}")
+	fun delete(@PathVariable id: Int): ResponseEntity<Any> =
 		billsService.delete(id).run {
 			ResponseEntity(HttpStatus.NO_CONTENT)
 		}
--- src/main/kotlin/com/talkdesk/billstracker/services/BillsService.kt
+++ src/main/kotlin/com/talkdesk/billstracker/services/BillsService.kt
@@ -3,7 +3,9 @@ package com.talkdesk.billstracker.services
 import com.talkdesk.billstracker.entities.Bill
 import com.talkdesk.billstracker.exceptions.NotFoundException
 import com.talkdesk.billstracker.repositories.BillsRepository
+import org.springframework.stereotype.Service
 
+@Service
 class BillsService(
 	private val billsRepository: BillsRepository
 ) {
